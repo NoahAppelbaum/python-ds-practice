37794954ed66c4709d596f5d1e9d02bc7a11d920
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-21 17:52:28 -0500

    1-5

--- 05_reverse_string.py
+++ 05_reverse_string.py
@@ -7,3 +7,9 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+    reversed_string = ""
+    for index in range(len(phrase)-1, -1, -1):
+        reversed_string += phrase[index]
+    return reversed_string
+
+# FIXME: This seems bad.
