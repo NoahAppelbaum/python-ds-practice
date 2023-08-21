7e4c3ec01a67099538f04767cb3ff51b21c68ee4
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-21 18:32:35 -0500

    tried to refactor #7

--- 07_multiple_letter_count.py
+++ 07_multiple_letter_count.py
@@ -16,3 +16,23 @@ def multiple_letter_count(phrase):
             counter[char] = 1
 
     return counter
+
+    # More exciting attempts below:
+
+    # CLOSE, but out of order??:
+
+    # counter = {}
+    # for char in set(phrase):
+    #     counter[char] = phrase.count(char)
+
+    # return counter
+
+    # TypeError: 'builtin_function_or_method' object is not subscriptable
+
+    # counter = {}
+    # for char in phrase:
+    #     if counter.get(char):
+    #         continue
+    #     else:
+    #         counter[char] = phrase.count[char]
+    # return counter
